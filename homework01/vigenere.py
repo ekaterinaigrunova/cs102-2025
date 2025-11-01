ee511020e1ff2b0ac6f2711e6d5bd1d73bb7c67a
ekaterinaigrunova <k.igrunova@gmail.com>
2025-11-01 17:52:52 +0300

    Реализована функция encrypt_vigenere() и decrypt_vigenere()

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,3 +1,6 @@
+"""
+Это шифр Виженера
+"""
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
@@ -9,7 +12,23 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    len_plaintext = len(plaintext)
+    len_keyword = len(keyword)
+    keyword = keyword.upper()
+    if len(plaintext) == 0:
+        return ""
+    if len_plaintext != len_keyword:
+        keyword = (len_plaintext // len_keyword) * keyword + keyword[: len_plaintext % len_keyword]
+
+    for i in range(len_plaintext):
+        symbol = plaintext[i]
+        shift = ord(keyword[i]) - 65
+        if symbol.isupper():
+            ciphertext += chr(65 + (ord(symbol) - 65 + shift) % 26)
+        elif symbol.islower():
+            ciphertext += chr(97 + (ord(symbol) - 97 + shift) % 26)
+        else:
+            ciphertext += symbol
     return ciphertext
 
 
@@ -24,5 +43,23 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    len_ciphertext = len(ciphertext)
+    len_keyword = len(keyword)
+    keyword = keyword.upper()
+    if len(ciphertext) == 0:
+        return ""
+    if len_ciphertext != len_keyword:
+        keyword = ((len_ciphertext // len_keyword) * keyword +
+                  keyword[: len_ciphertext % len_keyword])
+
+    for i in range(len_ciphertext):
+        symbol = ciphertext[i]
+        shift = ord(keyword[i]) - 65
+        if symbol.isupper():
+            plaintext += chr(65 + (ord(symbol) - 65 - shift) % 26)
+        elif symbol.islower():
+            plaintext += chr(97 + (ord(symbol) - 97 - shift) % 26)
+        else:
+            plaintext += symbol
+
     return plaintext
